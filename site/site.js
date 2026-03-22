82c66e720956247e0c6b1b15b6ad70b4e67ac626
GlassyBridge114 <188959481+GlassyBridge@users.noreply.github.com>
2026-03-22 03:52:30 +0300

    Fix: update faqbtn selector

--- site/site.js
+++ site/site.js
@@ -18,7 +18,7 @@ function glitchSwap() {
 
 setInterval(glitchSwap, 10000);
 
-const faqBtn = document.querySelector('a[href="faq.html"]');
+const faqBtn = document.querySelector('a');
 const faqModal = document.getElementById('faq-modal');
 const closeBtn = document.getElementById('close-faq');
 
